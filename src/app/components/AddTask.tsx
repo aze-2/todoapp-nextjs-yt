61e7ea507d7b7e46ce5c61bd4178a4a030bb0fca
azetsu <ten.nob.r6@gmail.com>
2025-02-04 06:48:27 +0900

    edit,deleteの追加、addtaskの修正

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { addTodo }  from '../pages/api';
+import { v4 as uuidv4 } from "uuid";
+
+const AddTask = () => {
+    const [taskTitle, setTaskTitle] = useState("");
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        const newTodo = await addTodo({ id:"uuidv4", text: taskTitle})
+
+
+        setTaskTitle("")
+        
+    }
+    
+
+    return (
+    <form className='mb-4 space-y-3' onSubmit={handleSubmit}>
+        <input type="text"
+        className='w-full border pe-4 py-2 rouded-lg focus:outline-none focus:border-blue-400' 
+        onChange={(e: ChangeEvent<HTMLInputElement>) => 
+            setTaskTitle(e.target.value)
+            }
+            value={taskTitle}
+        />
+        <button className='w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200' onClick={handleSubmit}>Add Task</button>
+    </form>
+  )
+}
+
+export default AddTask
---
+++ src/app/components/Todo.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import React, { useEffect, useRef, useState } from 'react'
+import { Task } from '../pages/types'
+import { deleteTodo, editTodo } from '../pages/api';
+
+interface TodoProps {
+    todo: Task;
+}
+
+const Todo = ({todo}:TodoProps) => {
+    const ref = useRef<HTMLInputElement>(null)
+
+    const [isEditing, setIsEditing] = useState(false)
+    const [editedTaskTitle, setEditedTaskTitle] = useState(todo.text)
+
+    useEffect(() => {
+        if(isEditing) {
+            ref.current?.focus()
+        }
+    },[isEditing])
+
+    const handleEdit = async () => {
+        setIsEditing(true)
+    }
+
+    const handleSave = async () => {
+        const updatedTodo = await editTodo(todo.id,editedTaskTitle);
+        console.log(updatedTodo)
+        setIsEditing(false)
+    }
+
+    const handleDelete = async () => {
+        await deleteTodo(todo.id)
+    }
+
+  return (
+    <li
+        key={todo.id}
+        className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rouded shadow'>
+    
+        {isEditing ? (
+            <input 
+            ref={ref}
+            type="text" 
+            className='mr-2py-1 px-2 rouded border-gray-400'
+            value={editedTaskTitle}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditedTaskTitle(e.currentTarget.value)} />
+            ) : (<span>{todo.text}</span>
+            )
+        }
+
+        <div>
+            {isEditing ? (<button className='text-blue-500 mr-3' onClick={handleSave}>save</button>
+            )　:　(<button className='text-green-500 mr-3' onClick={handleEdit}>edit</button>
+            )
+            }
+            <button className='text-red-500' onClick={handleDelete}>Delete</button>
+        </div>
+    </li>
+)
+}
+
+export default Todo
